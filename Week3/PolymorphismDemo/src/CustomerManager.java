9a69fc0b8b36aa99f3987625187b87cc65cb4273
bartupusat <117815967+bartupusat@users.noreply.github.com>
2022-11-21 16:32:03 +0300

    Add files via upload

---
+++ Week3/PolymorphismDemo/src/CustomerManager.java
@@ -0,0 +1,16 @@
+public class CustomerManager {
+
+    private BaseLogger logger;
+
+    public  CustomerManager(BaseLogger baseLogger){
+        this.logger = baseLogger;
+
+    }
+    public void add(){
+        System.out.println("Müşteri Eklendi");
+        this.logger.log("Log Mesajı");
+
+
+    }
+
+}
---
+++ Week3/overriding/src/HastaKrediManager.java
@@ -0,0 +1,5 @@
+public class HastaKrediManager extends BaseKrediManager{
+    public double hesapla(double tutar){
+        return tutar * 1;
+    }
+}
